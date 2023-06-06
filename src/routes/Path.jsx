4a6a463b56b4fcf47b137b371647cb2350a7a662
chanmyae-aung <chanmyaeaung.cmg@gmail.com>
2023-06-06 13:46:21 +0630

    route guard added

--- src/routes/Path.jsx
+++ src/routes/Path.jsx
@@ -4,15 +4,17 @@ import Register from '../pages/Register'
 import Login from '../pages/Login'
 import Dashboard from '../pages/Dashboard'
 import CreateContact from '../pages/CreateContact'
+import RouteGuard from '../components/RouteGuard'
+import LoginGuard from '../components/LoginGuard'
 
 const Path = () => {
   return (
     <div>
         <Routes>
-            <Route path='/' element={<Dashboard/>}/>
-            <Route path='/register' element={<Register/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/create' element={<CreateContact/>}/>
+            <Route path='/' element={<LoginGuard><Dashboard/></LoginGuard>}/>
+            <Route path='/register' element={<RouteGuard><Register/></RouteGuard>}/>
+            <Route path='/login' element={<RouteGuard><Login/></RouteGuard>}/>
+            <Route path='/create' element={<LoginGuard><CreateContact/></LoginGuard>}/>
         </Routes>
     </div>
   )
